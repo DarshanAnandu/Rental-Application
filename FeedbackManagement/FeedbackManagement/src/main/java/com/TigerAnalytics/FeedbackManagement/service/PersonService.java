1403889c7b2d243b1f39a99c7f7406e3f251d323
DarshanAnandu <84951893+DarshanAnandu@users.noreply.github.com>
2023-06-11 22:11:34 +0530

    Add files via upload

---
+++ FeedbackManagement/FeedbackManagement/src/main/java/com/TigerAnalytics/FeedbackManagement/service/PersonService.java
@@ -0,0 +1,34 @@
+package com.TigerAnalytics.FeedbackManagement.service;
+
+import com.TigerAnalytics.FeedbackManagement.dao.PersonDao;
+import com.TigerAnalytics.FeedbackManagement.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+    @PostMapping
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+}
